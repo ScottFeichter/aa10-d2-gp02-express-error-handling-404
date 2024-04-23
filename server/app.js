aa55b1faa37d75ad0b23688045aae4be477f33f7
Scott Feichter <scottfeichter@gmail.com>
2024-04-23 11:12:02 -0700

    successfully created catch all error

--- server/app.js
+++ server/app.js
@@ -5,5 +5,15 @@ app.get('/', (req, res) => {
   res.send('GET / This is the root URL');
 });
 
+// catch-all error handler to pre-empt the xprs built in
+app.use((req, res, next) => {
+  const err = new Error;
+  err.message = "Sorry the requested resourc couldn't be found";
+  err.statusCode = "404";
+  console.log(err.message);
+  console.log(err.statusCode);
+  next(err);
+});
+
 const port = 5000;
 app.listen(port, () => console.log('Server is listening on port', port));
